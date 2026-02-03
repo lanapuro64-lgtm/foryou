6ebfdc76160e26982ced6de37740ca6e5a2d6913
lanapuro64-lgtm <lanapuro64@gmail.com>
2026-02-03 12:09:53 +0800

    Fix question text formatting in config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me? :(",                                    // First interaction
+            text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No..",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you hehe! ❤️"           // Secret hover message
